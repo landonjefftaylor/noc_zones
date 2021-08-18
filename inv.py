be9d2d7b47d1cb10165d6a959a0dc359bd17c718
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2021-08-18 11:33:18 -0600

    improved

--- inv.py
+++ inv.py
@@ -17,5 +17,30 @@ with open('inv.txt','w') as ofile:
             # print(inv)
             ofile.write(inv + "\n")
             inv = "invariant "
+    
+    ofile.write("\n\n")
+
+    for y in range(len(zones)):
+        inv = inv + "zone(" + zones[y] + ").ok -> ("
+        for i in range(1,18):
+            inv = inv + "zone(" + zones[y] + ").c" + str(i).zfill(2) + " | "
+        inv = inv + "zone(" + zones[y] + ").c18) "
+        # print(inv)
+        ofile.write(inv + "\n")
+        inv = "invariant "
         
-        # invariant (zone(test).ok & zone(b).ok) -> (zone(test).c01 ~= zone(b).c01 | zone(test).c02 ~= zone(b).c02 | zone(test).c03 ~= zone(b).c03 | zone(test).c04 ~= zone(b).c04 | zone(test).c05 ~= zone(b).c05 | zone(test).c06 ~= zone(b).c06 | zone(test).c07 ~= zone(b).c07 | zone(test).c08 ~= zone(b).c08 | zone(test).c09 ~= zone(b).c09 | zone(test).c10 ~= zone(b).c10 | zone(test).c11 ~= zone(b).c11 | zone(test).c12 ~= zone(b).c12 | zone(test).c13 ~= zone(b).c13 | zone(test).c14 ~= zone(b).c14 | zone(test).c15 ~= zone(b).c15 | zone(test).c16 ~= zone(b).c16)
+    ofile.write("\n\n")
+    
+    for i in range(1,19):
+        inv = inv + "("
+        
+        for y in range(len(zones)-1):
+            inv = inv + "zone(" + zones[y] + ").ok & "
+        inv = inv + "zone(r).ok) -> ("
+        
+        for y in range(len(zones)-1):
+            inv = inv + "zone(" + zones[y] + ").c" + str(i).zfill(2) + " | "
+        inv = inv + "zone(r).c18)"
+        
+        ofile.write(inv + "\n")
+        inv = "invariant "
